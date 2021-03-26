c555e5187e74e59ff2878d0bf5493de98fc169a6
Patrick Ly <patrickly72@gmail.com>
2021-03-25 19:15:24 -0700

    declared function to shuffle array items

--- script1.js
+++ script1.js
@@ -110,3 +110,17 @@ function getPasswordLength(){
     
     }
     }
+
+// declare function to shuffle array
+//fisher-yates modern shuffle algorithm
+function shuffleArray(array){
+    for (var i =array.length-1; i>0; i--){
+var j= Math.floor(Math.random()*(i+1));
+var temp = array[i];
+array[i] = array[j];
+array[j] = temp;
+    }
+return array;
+};
+
+
